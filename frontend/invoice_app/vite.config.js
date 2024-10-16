7fbb333f12428586c9c7ae9b9a29367d26c4b5db
Permana Sibarani <permanakaisarmiraclesibarani@gmail.com>
2024-10-16 23:41:20 +0700

    adjust

--- frontend/invoice_app/vite.config.js
+++ frontend/invoice_app/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/",
+  base: "./", // Menggunakan path relatif
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", // Lokasi folder assets di dist
+  },
 });
